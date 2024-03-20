fe75424850b0306a2b68e032a61c10a3fc856d36
PurpleZool <m.royo@live.com>
2024-03-21 00:12:35 +0100

    First push. WorkingVersion

---
+++ GDITest/FrmTest.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmTest
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(12, 12)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(132, 23)
+        TextBox1.TabIndex = 0
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(12, 41)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(97, 19)
+        RadioButton1.TabIndex = 1
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "RadioButton1"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(12, 66)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(97, 19)
+        RadioButton2.TabIndex = 1
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "RadioButton1"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' FrmTest
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(170, 96)
+        Controls.Add(RadioButton2)
+        Controls.Add(RadioButton1)
+        Controls.Add(TextBox1)
+        Name = "FrmTest"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+End Class
---
+++ GDITest/GDIObj.vb
@@ -0,0 +1,24 @@
+﻿Public Class GDIObj
+    Dim _pen As Pen
+    Dim _g As Graphics
+
+    Public Sub New(pen As Pen, g As Graphics)
+        _pen = pen
+        _g = g
+    End Sub
+
+    Public Sub Dispose()
+        _pen.Dispose()
+        _g.Dispose()
+    End Sub
+
+End Class
+
+
+Public Class ThreadedRefreshEventArgs
+    Public _value As Integer
+
+    Public Sub New(value As Integer)
+        _value = value
+    End Sub
+End Class
